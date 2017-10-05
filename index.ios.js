c9cb3c962ed2db900929f26bac0be7f2a3594a5c
Salvador Hernandez <hernandezg.sal@gmail.com>
2017-10-05 13:33:02 -0700

    added store to app

--- index.ios.js
+++ index.ios.js
@@ -6,22 +6,39 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet
+    AppRegistry,
+    StyleSheet
 } from 'react-native';
 
 
 import App from './app/App'
+
+import { Provider } from 'react-redux/'
+
+import {createStore, applyMiddleware} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './app/reducers/index'
+import {persistStore, autoRehydrate} from 'redux-persist'
+var {AsyncStorage} = require('react-native');
+
+const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
+const store = autoRehydrate()(createStoreWithMiddleware)(rootReducer);
+// store.subscribe(() =>{
+//     console.log("store changed", store.getState())
+// })
+
 /**
  * Main Screen for IOS lands here, but we want both IOS and Android to show the same information.
  * This will return the "App"
  */
 export default class reactNativeIntro extends Component {
-  render() {
-    return (
-          <App />
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+              <App />
+            </Provider>
+        );
+    }
 }
 
 AppRegistry.registerComponent('reactNativeIntro', () => reactNativeIntro);
